9026a0c0937c7085fd45f98a0f271d69df6a8a00
ChromMob <filip.chromy04@gmail.com>
2022-12-26 14:48:47 +0100

    Initial commit

---
+++ src/main/java/me/chrommob/intaveadditions/IntaveAdditionsPlugin.java
@@ -0,0 +1,20 @@
+package me.chrommob.intaveadditions;
+
+import de.jpx3.intave.IntaveAccessor;
+import de.jpx3.intave.access.IntaveAccess;
+import de.jpx3.intave.access.IntaveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class IntaveAdditionsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
